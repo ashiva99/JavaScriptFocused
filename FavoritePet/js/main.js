28cdaa7f9ee35999485ea323fe89ce04ace56b65
ashiva99 <shiva5130@gmail.com>
2025-03-17 12:14:54 -0400

    Add initial HTML structure, CSS styles, and JavaScript functionality for Favorite Pet feature. Showing JS functionality by hilighting the image border.

---
+++ FavoritePet/js/main.js
@@ -0,0 +1,5 @@
+  
+  function changeBorderColor() {
+    image = document.querySelector('#pet-image');
+    image.style.borderColor = 'yellow'; // Here we are changing the border color of the image to yellow when the button is clicked
+  }
